8e1b819417c3e3bf56592e77a53a34b16b6cb970
17-08-2003 <120261965+17-08-2003@users.noreply.github.com>
2022-12-30 23:40:57 +0530

    business landing

---
+++ business landing.js
@@ -0,0 +1,15 @@
+function submitfunction(){
+    let FirstName=document.getElementById('firstname').value;
+    let LastName=document.getElementById('lastname').value;
+    let Email=document.getElementById('email').value;
+    let Reason=document.getElementById('reason').value;
+
+    console.log(FirstName,LastName,Email,Reason);
+
+     if(FirstName && LastName && Email && Reason)
+     {
+        alert("Form Submitted Successfully");
+    }else{
+        alert("All fields are required");
+    }
+}
